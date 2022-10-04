e25648659ce01beea8bbc4b30c024df2a2d0568b
julgrabar <45499113+julgrabar@users.noreply.github.com>
2022-10-04 14:43:53 +0300

    bottom padding fix

--- src/styles/Container.ts
+++ src/styles/Container.ts
@@ -4,7 +4,7 @@ export const Container = styled.div`
   max-width: 480px;
   min-height: calc(100vh - 72px);
   height: 1px;
-  padding: 30px;
+  padding: 30px 30px 0 30px;
   width: 100%;
   margin: 0 auto;
   @media screen and (min-width: 768px) {
